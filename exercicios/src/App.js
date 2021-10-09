cd1af83822de8c47063a16d5dc8b0b56f5ab8554
Marcio Rela <marciorela75@gmail.com>
2021-10-09 10:27:36 -0300

    Desenvolvendo exercicios

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,10 +1,31 @@
 import React from 'react'
 // import { Text } from 'react-native'
-import { View } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 
+import MinMax from './components/MinMax'
+import X, { Comp1, Comp2 } from './components/Multi'
 import Primeiro from './components/Primeiro'
 
-export default () => <View><Primeiro/></View>
+export default () => (
+    <View style={style.App}>
+        <MinMax min={3} max={20} />
+        <MinMax min={20} max={94} />
+        {/* <X/>
+        <Comp1/>
+        <Comp2/>
+        <Primeiro/> */}
+    </View>
+)
+
+const style = StyleSheet.create({
+    App: {
+        //backgroundColor: '#A00',
+        flexGrow: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20
+    }
+})
 
 // export default () => {
 //     return <Text>Primeiro Componente!!!</Text>
