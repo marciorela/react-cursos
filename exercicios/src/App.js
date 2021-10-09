aeeac99beba6ebd9189daaadd533429e99be5665
Marcio Rela <marciorela75@gmail.com>
2021-10-09 09:04:04 -0300

    Desenvolvendo 'exercicios'

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,8 +1,28 @@
 import React from 'react'
-import { Text } from 'react-native'
+// import { Text } from 'react-native'
+import { View } from 'react-native'
 
-function App() {
-    return <Text>Primeiro Componente</Text>
-}
+import Primeiro from './components/Primeiro'
 
-export default App
+export default () => <View><Primeiro/></View>
+
+// export default () => {
+//     return <Text>Primeiro Componente!!!</Text>
+// }
+
+// OU...
+// export default function () {
+//     return <Text>Primeiro Componente</Text>
+// }
+
+// OU...
+// const App = function () {
+//     return <Text>Primeiro Componente</Text>
+// }
+// export default App
+
+// OU...
+// function App() {
+//     return <Text>Primeiro Componente</Text>
+// }
+// export default App
