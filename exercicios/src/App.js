ada87947ac18043eab67fa39085a67e9451a05a0
Marcio Rela <marciorela75@gmail.com>
2021-10-10 17:25:15 -0300

    Andamento do curso

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,19 +1,33 @@
 import React from 'react'
-// import { Text } from 'react-native'
 import { View, StyleSheet } from 'react-native'
 
-import MinMax from './components/MinMax'
-import X, { Comp1, Comp2 } from './components/Multi'
-import Primeiro from './components/Primeiro'
+//import Pai from './components/direta/Pai'
+
+import Contador from './components/Contador'
+// import Botao from './components/Botao'
+// import Titulo from './components/Titulo'
+// import MinMax from './components/MinMax'
+// import X, { Comp1, Comp2 } from './components/Multi'
+// import Primeiro from './components/Primeiro'
+// import Aleatorio from './components/Aleatorio'
 
 export default () => (
     <View style={style.App}>
+
+        <Contador inicial={100} passo={3} />
+        {/* 
+        <Pai />
+        <Contador />
+        <Botao/>
+        <Titulo principal="Cadastro de Produto" secundario="Tela de Cadastro de Produto"/>
+        <Aleatorio min={1} max={60} />
         <MinMax min={3} max={20} />
         <MinMax min={20} max={94} />
-        {/* <X/>
+        <X/>
         <Comp1/>
         <Comp2/>
-        <Primeiro/> */}
+        <Primeiro/>
+        */}
     </View>
 )
 
