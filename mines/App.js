9e376f93ca4b0c28c480b11b4cd4f734044ac463
Marcio Rela <marciorela75@gmail.com>
2021-10-24 11:09:04 -0300

    Projeto: mines [em andamento]

---
+++ mines/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import params from './src/params';
+import MineField from './src/components/MineField';
+import { createMinedBoard } from './src/functions';
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = this.createState()
+  }
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount();
+    const rows = params.getRowsAmount();
+    
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  createState = () => {
+    const cols = params.getColumnsAmount();
+    const rows = params.getRowsAmount();
+
+    return {
+      board: createMinedBoard(rows, cols, this.minesAmount()),
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Iniciando o Mines!</Text>
+        <Text style={styles.introduction}>
+          Tamanho da grade:
+          {params.getRowsAmount()} x {params.getColumnsAmount()}
+        </Text>
+
+        <View style={styles.board}>
+          <MineField board={this.state.board}/>
+        </View>
+
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+
+  board: {
+    alignItems: 'center',
+    backgroundColor: '#AAA'
+  }
+})
